19c8ca20fa474c57eb3912c5b4da108d6c248b08
Max Blum <maxpblum@users.noreply.github.com>
2015-02-04 17:45:38 -0500

    Simplified

--- crx/semit.js
+++ crx/semit.js
@@ -2,9 +2,6 @@
   var headings = document.getElementsByClassName("story-heading");
 
   Array.prototype.forEach.call(headings, function (heading) {
-    var nytnow = heading.innerText.indexOf("NYT Now") != -1;
-    var tmp = nytnow ? heading.innerText.slice(null, -7) : heading.innerText;
-    tmp = tmp.split("").reverse().join("");
-    heading.innerText = nytnow ? tmp + "NYT Now" : tmp;
+    heading.innerText = heading.innerText.split("").reverse().join("");
   });
 }());
